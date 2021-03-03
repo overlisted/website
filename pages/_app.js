34390f0f6e09553467bf4c8d0da9da31e8c9ec77
Ilya Maximov <mail@overlisted.net>
2021-03-03 23:45:40 +0100

    add extra metadata

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,10 @@ const App = ({ Component, pageProps }) => {
   return <>
     <Head>
       <title>overlisted</title>
+      <meta name="author" content="overlisted, mail@overlisted.net" />
+      <meta name="description" content="My personal website" />
+      <meta name="color-scheme" content="only light" />
+      <meta name="robots" content="all" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <Component {...pageProps} />
